71916c53ee59d161319750a058b9208083f55726
KelvinWillian757 <83711126+KelvinWillian757@users.noreply.github.com>
2026-03-02 17:54:00 -0300

    Update CETIPO.py

--- scripts/CETIPO.py
+++ scripts/CETIPO.py
@@ -1,3 +1,6 @@
+def main():
+    print("Marcando registro como NULL em:")
+    # aqui vai todo o seu código do ETL
 
-print("Marcando registro como NULL em:")
-
+if __name__ == "__main__":
+    main()
